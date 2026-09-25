6e6d74da864236d3945051a97009c821dce1efc7
xlassaj00 <xlassaj00@vutbr.cz>
2026-09-25 14:18:14 +0200

    test5

--- test5.c
+++ test5.c
@@ -1,5 +1,27 @@
+//rozlisovanie sudych a lichych cisel s opakovanim
 #include <stdio.h>
-int main()
+int main(void)
 {
     
+    int cislo;
+    int pocet_opakovani;
+
+    printf("zadaj kolko cisel chces checknut ci su sude alebo liche:\n");
+    scanf("%d", &pocet_opakovani);
+
+    for (int i=0; i<pocet_opakovani; i++)
+    {
+        
+        printf("zadaj cislo: \n");
+        scanf("%d", &cislo);
+        if (cislo%2 == 0)
+        {
+            printf("cislo je sude\n");
+        }
+        else
+        {
+            printf("cislo je liche\n");
+        }
+    }
+    return 0;
 }
