1560e0cbc13e2c4d87fa55dc0398c81fc92b7df7
xlassaj00 <xlassaj00@vutbr.cz>
2026-09-26 16:53:29 +0200

    test10

--- makefile
+++ makefile
@@ -1,8 +1,8 @@
 CC = gcc
 CFLAGS = -std=c23 -Wall -Wextra -Werror 
 LDFLAGS = -lm
-TARGET = test9
-SRC = test9.c
+TARGET = test10
+SRC = test10.c
 
 $(TARGET): $(SRC)
 	$(CC) $(CFLAGS) -o $(TARGET) $(SRC) $(LDFLAGS)
---
+++ test10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int hodiny, minuty, sekundy, hodiny_v_sekundach, minuty_v_sekundach;
+
+    
+    printf("zadajte cas v hodinach a minutach oddelene:\n");
+    scanf("%d %d", &hodiny, &minuty);
+
+
+    hodiny_v_sekundach = hodiny*360;
+    minuty_v_sekundach = minuty*60;
+    sekundy = hodiny_v_sekundach + minuty_v_sekundach;
+
+
+    printf("%d h je %d sekund, %d min je %d sekund a celkovo je to %d sekund\n", hodiny, hodiny_v_sekundach, minuty, minuty_v_sekundach, sekundy);
+    
+
+
+    return 0;
+}
